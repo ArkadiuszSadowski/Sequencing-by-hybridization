d6427cd8db4a9a0d19e4301056c02bc2ee9b0cbf
Elisa Mamos <temerair@op.pl>
2018-12-11 13:31:40 +0100

    Firt attempt
    Creating complete graph from spectrum  + sorting neighbours 
    Prepared for TSP algorithm implementation.

---
+++ bioInf/bioInf/Node.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bioInf
+{
+    class Node
+    {
+        private string value;
+       private int coverage;
+        
+        private List<Node> neighbourConnections;
+         public  Node(string value)
+         {
+            neighbourConnections = new List<Node>();
+           
+            this.value = value;
+         }
+
+        private void setCoverage(int coverage)
+        {
+            this.coverage = coverage;
+        }
+
+        public void addNeighbour(string neighbour)
+        {
+            Node n = new Node(neighbour);
+           
+            int coverage = getCoverage(neighbour);
+            n.setCoverage(coverage);
+            neighbourConnections.Add(n);
+           
+        }
+
+        private int getCoverage(string neighbourValue)
+        {
+            int coverage = 0;
+            if (neighbourValue.Equals(value))
+            {
+                coverage = neighbourValue.Length;
+                return coverage;
+            }
+            else
+            {
+                for (int i = 1; i < value.Length; i++)
+                {
+
+                    if (value.Substring(i).Equals(neighbourValue.Substring(0, neighbourValue.Length - i)))
+                    {
+                        coverage = neighbourValue.Length - i;
+                        return coverage;
+                    }
+
+
+                }
+        }
+
+            return coverage;
+
+
+        }
+
+        public int getSavedCoverageValue()
+        {
+            return coverage;
+        }
+
+        public List<Node> getNeighbours()
+        {
+            return neighbourConnections;
+        }
+
+        public string getValue()
+        {
+            return value;
+        }
+
+        public void sortNeighbours()
+        {
+         
+            Node temp;
+
+            for (int write = 0; write < neighbourConnections.Count; write++)
+            {
+                for (int sort = 0; sort < neighbourConnections.Count - 1; sort++)
+                {
+                    if (neighbourConnections[sort].getSavedCoverageValue() > neighbourConnections[sort + 1].getSavedCoverageValue())
+                    {
+                        temp = neighbourConnections[sort + 1];
+                        neighbourConnections[sort + 1] = neighbourConnections[sort];
+                        neighbourConnections[sort] = temp;
+                    }
+                }
+            }
+
+            neighbourConnections.Reverse(0, neighbourConnections.Count);
+
+        }
+
+        public void printNeighbours()
+        {
+            sortNeighbours();
+            foreach (Node n in neighbourConnections)
+            {
+               
+                Console.Write( "Neighbour = {0}, Coverage = {1}", n.getValue(), n.getSavedCoverageValue());
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ bioInf/bioInf/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bioInf
+{
+    class Program
+    {
+        private static List<Node> graph;
+        static void Main(string[] args)
+        {
+            string[] spectrum ={"ACT", "CTC", "GCC", "TCT", "TGG"};
+            graph = new List<Node>();
+            createCompleteGraph(spectrum);
+            
+            // Node n = new Node("CTC");
+            //n.addNeighbour("TCD");
+            //n.printNeighbours();
+
+            Console.Read();
+        }
+
+        public static void createCompleteGraph(string[]spectrum)
+        {
+            int position = 0;
+            foreach (var word in spectrum)
+            {   
+                Node n = new Node(word);
+                for (int i = 0; i < spectrum.Length; i++)
+                {
+                    if (position != i)
+                    {
+                        n.addNeighbour(spectrum[i]);
+                    }
+
+               
+                }
+                position++;
+                graph.Add(n);
+
+
+            }
+
+            printGraph();
+        }
+
+        public static void printGraph()
+        {
+            foreach (var node in graph)
+            {
+                Console.Write(node.getValue()+"    ");
+               
+                node.printNeighbours();
+                Console.Write("\n");
+            }
+
+        }
+
+        private void getPossibleWays()
+        {   
+            
+        }
+        
+
+        public class PossibleConnection
+        {
+            private string superString;
+            private List<Node> mergedList;
+            private int cover;
+            public PossibleConnection(string superstring)
+            {
+                this.superString = superstring;
+                mergedList = new List<Node>();
+                cover = 0;
+
+            }
+
+            public string getSuperString()
+            {
+                return superString;
+            }
+
+            public void setSuperString(string superString)
+            {
+                this.superString = superString;
+            }
+            public List<Node> getCoveredNodes()
+            {
+                return mergedList;
+            }
+
+            public void addCoveredNodes(Node node)
+            {
+               mergedList.Add(node);
+            }
+            public int getSumCoverage()
+            {
+                return cover;
+            }
+
+            public void addSumCoverage(int coverage)
+            {
+                cover += coverage;
+            }
+        }
+
+    }
+}
